28f921be31256766d546348925a305b89c0858d5
Seaaaannnnn <seanforlanda@gmail.com>
2024-09-24 16:08:56 +0800

    Changes some script of the submit-archive

--- inc/footer.php
+++ inc/footer.php
@@ -61,18 +61,17 @@
     }
   })
 </script>
-<footer class="main-footer text-sm">
+<!-- <footer class="main-footer text-sm">
   <div class="container">
         <strong>Copyright © <?php echo date('Y') ?>. 
-        <!-- <a href=""></a> -->
         </strong>
         All rights reserved.
         <div class="float-right d-none d-sm-inline-block">
-          <b><?php echo $_settings->info('short_name') ?> (by: <a href="mailto:oretnom23@gmail.com" target="blank">oretnom23</a> )</b> v1.0
+          <b><?php echo $_settings->info('short_name') ?> (at: <a href="mailto:seanforlanda@gmail.com" target="blank">PUPSRC</a> )</b>
         </div>
       </div>
       </footer>
-    </div>
+    </div> -->
     <!-- ./wrapper -->
 <div id="libraries">
     <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
--- submit-archive.php
+++ submit-archive.php
@@ -4,7 +4,7 @@ if(isset($_GET['id']) && $_GET['id'] > 0){
     if($qry->num_rows){
         foreach($qry->fetch_array() as $k => $v){
             if(!is_numeric($k))
-            $$k = $v;
+                $$k = $v;
         }
     }
     if(isset($student_id)){
@@ -22,6 +22,7 @@ if(isset($_GET['id']) && $_GET['id'] > 0){
         width:calc(100%);
 	}
 </style>
+
 <div class="content py-4">
     <div class="card card-outline card-primary shadow rounded-0">
         <div class="card-header rounded-0">
@@ -34,8 +35,8 @@ if(isset($_GET['id']) && $_GET['id'] > 0){
                     <div class="row">
                         <div class="col-lg-6">
                             <div class="form-group">
-                                <label for="title" class="control-label text-navy">Project Title</label>
-                                <input type="text" name="title" id="title" autofocus placeholder="Project Title" class="form-control form-control-border" value="<?= isset($title) ?$title : "" ?>" required>
+                                <label for="title" class="control-label text-navy">Research Title</label>
+                                <input type="text" name="title" id="title" autofocus placeholder="Project Title" class="form-control form-control-border" value="<?= isset($title) ? $title : "" ?>" required>
                             </div>
                         </div>
                     </div>
@@ -45,9 +46,9 @@ if(isset($_GET['id']) && $_GET['id'] > 0){
                                 <label for="year" class="control-label text-navy">Year</label>
                                 <select name="year" id="year" class="form-control form-control-border" required>
                                     <?php 
-                                        for($i= 0;$i < 51; $i++):
+                                        for($i= 0; $i < 51; $i++):
                                     ?>
-                                    <option <?= isset($year) && $year == date("Y",strtotime(date("Y")." -{$i} years")) ? "selected" : "" ?>><?= date("Y",strtotime(date("Y")." -{$i} years")) ?></option>
+                                    <option <?= isset($year) && $year == date("Y", strtotime(date("Y")." -{$i} years")) ? "selected" : "" ?>><?= date("Y", strtotime(date("Y")." -{$i} years")) ?></option>
                                     <?php endfor; ?>
                                 </select>
                             </div>
@@ -84,7 +85,7 @@ if(isset($_GET['id']) && $_GET['id'] > 0){
                     <div class="row">
                         <div class="col-lg-12">
                             <div class="form-group">
-                                <label for="pdf" class="control-label text-muted">Project Document (PDF File Only)</label>
+                                <label for="pdf" class="control-label text-muted">Project Document (PDF File Only maximum of 25mb)</label>
                                 <input type="file" id="pdf" name="pdf" class="form-control form-control-border" accept="application/pdf" <?= !isset($id) ? "required" : "" ?>>
                             </div>
                         </div>
@@ -92,7 +93,7 @@ if(isset($_GET['id']) && $_GET['id'] > 0){
                     <div class="row">
                         <div class="col-lg-12">
                             <div class="form-group text-center">
-                                <button class="btn btn-default bg-navy btn-flat"> Update</button>
+                                <button class="btn btn-default bg-navy btn-flat"> Upload</button>
                                 <a href="./?page=profile" class="btn btn-light border btn-flat"> Cancel</a>
                             </div>
                         </div>
@@ -102,88 +103,109 @@ if(isset($_GET['id']) && $_GET['id'] > 0){
         </div>
     </div>
 </div>
-<script>
-    function displayImg(input,_this) {
-	    if (input.files && input.files[0]) {
-	        var reader = new FileReader();
-	        reader.onload = function (e) {
-	        	$('#cimg').attr('src', e.target.result);
-	        }
 
-	        reader.readAsDataURL(input.files[0]);
-	    }else{
+<script>
+    function displayImg(input, _this) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#cimg').attr('src', e.target.result);
+            }
+            reader.readAsDataURL(input.files[0]);
+        } else {
             $('#cimg').attr('src', "<?= validate_image(isset($avatar) ? $avatar : "") ?>");
         }
-	}
-    $(function(){
+    }
+
+    $(function() {
         $('.summernote').summernote({
             height: 200,
             toolbar: [
-                [ 'style', [ 'style' ] ],
-                [ 'font', [ 'bold', 'italic', 'underline', 'strikethrough', 'superscript', 'subscript', 'clear'] ],
-                [ 'fontname', [ 'fontname' ] ],
-                [ 'fontsize', [ 'fontsize' ] ],
-                [ 'color', [ 'color' ] ],
-                [ 'para', [ 'ol', 'ul', 'paragraph', 'height' ] ],
-                [ 'table', [ 'table' ] ],
+                ['style', ['style']],
+                ['font', ['bold', 'italic', 'underline', 'strikethrough', 'superscript', 'subscript', 'clear']],
+                ['fontname', ['fontname']],
+                ['fontsize', ['fontsize']],
+                ['color', ['color']],
+                ['para', ['ol', 'ul', 'paragraph', 'height']],
+                ['table', ['table']],
                 ['insert', ['link', 'picture']],
-                [ 'view', [ 'undo', 'redo', 'help' ] ]
+                ['view', ['undo', 'redo', 'help']]
             ]
-        })
+        });
+
         $('.summernote-list-only').summernote({
             height: 200,
             toolbar: [
-                [ 'font', [ 'bold', 'italic', 'clear'] ],
-                [ 'fontname', [ 'fontname' ] ]
-                [ 'color', [ 'color' ] ],
-                [ 'para', [ 'ol', 'ul' ] ],
-                [ 'view', [ 'undo', 'redo', 'help' ] ]
+                ['font', ['bold', 'italic', 'clear']],
+                ['fontname', ['fontname']],
+                ['color', ['color']],
+                ['para', ['ol', 'ul']],
+                ['view', ['undo', 'redo', 'help']]
             ]
-        })
+        });
+
         // Archive Form Submit
-        $('#archive-form').submit(function(e){
-            e.preventDefault()
-            var _this = $(this)
-                $(".pop-msg").remove()
-            var el = $("<div>")
-                el.addClass("alert pop-msg my-2")
-                el.hide()
+        $('#archive-form').submit(function(e) {
+            e.preventDefault();
+            var _this = $(this);
+            $(".pop-msg").remove();
+            var el = $("<div>");
+            el.addClass("alert pop-msg my-2");
+            el.hide();
+
             start_loader();
+
+            if ($('#abstract').is(':hidden')) {
+                $('#abstract').removeAttr('required');
+            }
+            if ($('#members').is(':hidden')) {
+                $('#members').removeAttr('required');
+            }
+
+            if ($('#abstract').is(':hidden')) {
+                $('#abstract').show();
+            }
+            if ($('#members').is(':hidden')) {
+                $('#members').show();
+            }
+
             $.ajax({
-                url:_base_url_+"classes/Master.php?f=save_archive",
+                url: _base_url_ + "classes/Master.php?f=save_archive",
                 data: new FormData($(this)[0]),
                 cache: false,
                 contentType: false,
                 processData: false,
                 method: 'POST',
-                type: 'POST',
-                dataType:'json',
-                error:err=>{
-                    console.log(err)
-                    el.text("An error occured while saving the data")
-                    el.addClass("alert-danger")
-                    _this.prepend(el)
-                    el.show('slow')
-                    end_loader()
-                },
-                success:function(resp){
-                    if(resp.status == 'success'){
-                        location.href= "./?page=view_archive&id="+resp.id
-                    }else if(!!resp.msg){
-                        el.text(resp.msg)
-                        el.addClass("alert-danger")
-                        _this.prepend(el)
-                        el.show('show')
-                    }else{
-                        el.text("An error occured while saving the data")
-                        el.addClass("alert-danger")
-                        _this.prepend(el)
-                        el.show('show')
-                    }
+                dataType: 'json',
+                error: function(err) {
+                    console.log("AJAX error: ", err);
+                    el.text("An error occurred while saving the data: " + err.statusText);
+                    el.addClass("alert-danger");
+                    _this.prepend(el);
+                    el.show('slow');
                     end_loader();
-                    $('html, body').animate({scrollTop: 0},'fast')
+                },
+                success: function(resp) {
+                    console.log(resp); // Add this to see the full response
+
+                    if (resp.status == 'success') {
+                        location.href = "./?page=view_archive&id=" + resp.id;
+                    } else if (resp.msg) {
+                        el.text(resp.msg);
+                        el.addClass("alert-danger");
+                        _this.prepend(el);
+                        el.show('show');
+                    } else {
+                        el.text("An error occurred while saving the data");
+                        el.addClass("alert-danger");
+                        _this.prepend(el);
+                        el.show('show');
+                    }
+
+                    end_loader();
+                    $('html, body').animate({ scrollTop: 0 }, 'fast');
                 }
-            })
-        })
-    })
+            });
+        });
+    });
 </script>
